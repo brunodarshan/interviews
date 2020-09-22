d3998b7a7800fc4db6fd4473eb85a551672245b2
Darshan <brunodarshan@gmail.com>
2020-09-22 18:33:57 -0300

    add validation to advertiser_name, url, description, starts_at

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,8 @@
 
 # Offer model instance
 class Offer < ApplicationRecord
-  validates :advertiser_name, :url, presence: true
+  validates :advertiser_name, :url, :description, :starts_at, presence: true
   validates :advertiser_name, uniqueness: true
+  validates :description, length: { maximum: 500 }
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp, message: 'must be valid url' }
 end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -3,21 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe Offer do
-  describe '#valid?' do
-    let(:offer) { Offer.new }
-    context 'invalid' do
-      context 'when advertiser_name and/or url are empty' do
-        it { expect(offer.valid?).to be false }
-      end
+  let(:offer) { Offer.new }
+  context 'invalid' do
+    before { offer.valid? }
+
+    it '#valid returns false' do
+      expect(offer.valid?).to be false
     end
 
-    context 'valid' do
-      before do
-        offer.advertiser_name = 'Foo Ltda.'
-        offer.url = 'http://foo.com/buy_it'
+    describe '#errors' do
+      let(:expected_messages) do
+        {
+          advertiser_name: ['can\'t be blank'],
+          description: ['can\'t be blank'],
+          starts_at: ['can\'t be blank'],
+          url: ['can\'t be blank', 'must be valid url']
+        }
       end
-
-      it { expect(offer.valid?).to be true }
+      it { expect(offer.errors).to a_kind_of(ActiveModel::Errors) }
+      it {
+        expect(offer.errors.messages).to eql(expected_messages)
+      }
     end
+  end
+
+  context 'valid' do
+    before do
+      offer.advertiser_name = 'Foo Ltda.'
+      offer.url = 'http://foo.com/buy_it'
+      offer.description = 'lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.'
+      offer.starts_at = Time.now.utc
+    end
+
+    it '#valid returns false' do expect(offer.valid?).to be true end
+    it { expect(offer.errors).to be_empty }
   end
 end

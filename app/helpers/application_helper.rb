b8c40a4f80c66105ef2c46da2dd4a0ae755ec283
Darshan <brunodarshan@gmail.com>
2020-09-24 08:35:27 -0300

    add helper to validate errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def error_message(obj, key)
+    if obj.errors.messages.keys.include?(key)
+       %Q(<small class="text-danger porra"> #{obj.errors.messages[key].join(', ')} </small>).html_safe
+    end
+  end
 end
